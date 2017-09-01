47606fae6192356d922d58f031473ccf58b3fc10
Myron Anthony <codehearted12@gmail.com>
2017-09-01 15:11:17 -0400

    commit

---
+++ src/components/baselayout.js
@@ -0,0 +1,11 @@
+import Footer from './Footer'
+import Header from './Header'
+import React, { Component } from 'react'
+
+class BaseLayout extends Component {
+  render() {
+    return (
+      <div>This should house Header and Footer components and be able to house any children components.</div>
+    );
+  }
+}
---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+
+class Header extends Component {
+  render() {
+    return (
+      <nav>I am the Navigation Bar</nav>
+    );
+  }
+}
