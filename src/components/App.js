0b82aaf287da9d1af811c42b86608b3e7d194d55
Myron Anthony <codehearted12@gmail.com>
2017-09-22 14:46:28 -0400

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import '../styles/App.css';
 import ParentComponent from './ParentComponent'
 import BaseLayout from './BaseLayout'
-
+import Footer from './Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <BaseLayout>
+        <BaseLayout/>
 
-        <ParentComponent />
-      </BaseLayout>
+         <ParentComponent />
+         <Footer/>
       </div>
     );
   }
